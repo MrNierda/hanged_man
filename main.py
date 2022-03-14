9e389559aafe38c2d85de0fd7941e7167d95593f
MrNierda <mrnierda.courses@gmail.com>
2022-03-14 22:50:08 +0100

    first step to manage a game of hm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
+from time import sleep
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-import random
-from hangedMan import hanged_man
 from hangedMan.hanged_man import HangedManGame
 
 
@@ -14,9 +13,12 @@ class Main(App):
 @dataclass
 class MyRoot(BoxLayout):
     hanged_man_game = HangedManGame()
+    widgets: dict = None
 
     def __init__(self):
         super(MyRoot, self).__init__()
+        self.widgets = {}
+        self.hidden_word.text = "First give your name"
 
     def ask_user_name(self):
         print("ask user name")
@@ -25,17 +27,34 @@ class MyRoot(BoxLayout):
         player_name = str(self.user_input()).capitalize()
         print(f"{self.guess_inpt.text}")
         self.hanged_man_game.player = player_name
-        self.hidden_word_text = f"Hello {player_name}, let's start"
         
-
     def start_game(self):
-        # Get the item with the id 'random_label' as declared inside the .kv file
-        # TypeCasting to str required to avoid an error
         self.hidden_word.text = self.hanged_man_game.word_hidden
+        self.show_widget("guessInput")
+        self.show_widget("guessSubmit")
 
-    def user_input(self, v_input):
-        print(f"submit input: {v_input.text}")
-        return v_input.text
+    def get_user_name(self, user_name):
+        self.hide_widget(user_name)
+        self.hanged_man_game.player = user_name.text
+        self.start_game()
+
+    def hide_widget(self, widget):
+        widget_attributes = {
+            "opacity": widget.opacity,
+            "disabled": widget.disabled,
+            "height": widget.height,
+            "size_hint_y": widget.size_hint_y
+        }
+        self.widgets[widget.name] = widget_attributes
+
+        widget.opacity = 0
+        widget.disabled = True
+        widget.height = 0
+        widget.size_hint_y = None
+
+    def show_widget(self, widget_name): 
+        print(f"widget in layout {self.ids.layout.names}")
+        return
 
 
 testApp = Main()
